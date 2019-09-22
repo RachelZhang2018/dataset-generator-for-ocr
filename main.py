798a5b80a33dcefb8c0c3f680c529e25083892ac
Rui Zhang <201803717@post.au.dk>
2019-09-22 18:00:01 +0200

    update

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
+import argparse
+import training_gen
 
+parser = argparse.ArgumentParser(description='Generate training dataset.')
 
+parser.add_argument('-l', '-len', '--len', default=10, type=int,
+                    help='provide length of digital sequence (default: 10)')
+parser.add_argument('-n', '--n', default=1000, type=int,
+                    help='provide number of images (default: 1000)')
+parser.add_argument('-w', '--w', default=300, type=int,
+                    help='provide number of images (default: 300)')
+parser.add_argument('-min', '--min', default=0, type=int,
+                    help='provide minimum spacing between digits (default: 0)')
+parser.add_argument('-max', '--max', default=5, type=int,
+                    help='provide maximum spacing between digits (default: 5)')
 
+args = parser.parse_args()
+
+len_dig = args.len
+num_img = args.n
+image_width = args.w
+min_spacing = args.min
+max_spacing = args.max
+
+training_gen.images_generator(len_dig = len_dig, 
+							num_img = num_img, 
+							image_width = image_width, 
+							min_spacing = min_spacing, 
+							max_spacing = max_spacing)
 
--- training_gen.py
+++ training_gen.py
@@ -23,18 +23,6 @@ img_rows, img_cols = SIZE, SIZE
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
-'''
-def dig2img(digit, x_train, y_train):
-	# a filter to find the indices of images belonging to class i
-	fil = y_train[:, digit] == 1
-	indices = fil.nonzero()[0]
-	# find a random image from digits belonging to class i
-	num_img = indices.size
-	rand = np.random.randint(0, num_img)
-	rand_img_index = indices[rand]
-	rand_img = x_train[rand_img_index].reshape((SIZE, SIZE))
-	return rand_img
-'''
 
 def create_digit_sequence(number, image_width, min_spacing, max_spacing):
 	""" A function that create an image representing the given number,
@@ -88,6 +76,7 @@ def create_digit_sequence(number, image_width, min_spacing, max_spacing):
 
 	# convert new image to array
 	res = np.asarray(new_img)
+	show_digits(res)
 
 	return res
 
@@ -103,28 +92,41 @@ for i in range(10):
 '''
 
 # generate random digits sequence and save as csv file
-def digits_generator(num_dig):
-	numbers = np.random.randint(10, size = num_dig)
+def digits_generator(len_dig):
+	numbers = np.random.randint(10, size = len_dig)
 	res = ''.join([str(x) for x in numbers])
 	return res
 
-def images_generator(num_dig, num_img, image_width, min_spacing, max_spacing):
-	name = "train_len" + str(num_dig)
+def images_generator(len_dig, num_img, image_width, min_spacing, max_spacing):
+	name = "train_len" + str(len_dig)
+	nameLab = "lable_len" + str(len_dig)
 	f = open('data/{0}.csv'.format(name), 'wb')
+	f_l = open('data/{0}.csv'.format(nameLab), 'w')
+
 	
 	for i in range(num_img):
-		digits = digits_generator(num_dig)
+		digits = digits_generator(len_dig)
 		img = create_digit_sequence(digits, image_width, min_spacing, max_spacing)
 		img = img.reshape((1,-1))
 		np.savetxt(f, img, fmt='%i', delimiter = ",")
+		np.savetxt(f_l, [digits], fmt='%s')
+
 	f.close()
+	f_l.close()
 	
+	'''
+	# test: read labels
+	with open('data/{0}.csv'.format(nameLab)) as csvfile:
+		reader = csv.reader(csvfile)
+		for row in reader:
+			print(row)
+	'''
 
 dirName = 'data'
 if not os.path.exists(dirName):
 	os.mkdir(dirName)
 
-images_generator(5, 1, 200, 5, 10)
+# images_generator(len_dig = 5, num_img = 5, image_width = 200, min_spacing = 5, max_spacing = 10)
 
 '''
 # test
